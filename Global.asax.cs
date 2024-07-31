45b6aee6d02c7ef716029dd2b2c15e762c028c88
guga-ga <gugaga@tutamail.com>
2024-08-01 00:19:44 +0300

    minor fixes

---
+++ Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using ChampionshipMvc3.Infrastructure;
+using ChampionshipMvc3.Models;
+using ChampionshipMvc3.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ChampionshipMvc3
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "FootballSearchPage", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            InitializaDataContext();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+        }
+
+        private void InitializaDataContext()
+        {
+            PlayNowDataContext dataContext = new PlayNowDataContext();
+            RepositoryBase repositoryBase = new RepositoryBase(dataContext);
+        }
+    }
+}
